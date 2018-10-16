6066e84aa7cc7a777d0d790f29382709e6392bd7
christianirvin <irvinvela@hotmail.com>
2018-10-15 19:16:24 -0500

    added

--- app/src/main/java/com/kotlin/cursos/cursos/MainActivity.kt
+++ app/src/main/java/com/kotlin/cursos/cursos/MainActivity.kt
@@ -2,6 +2,8 @@ package com.kotlin.cursos.cursos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
                     "\n2. " + getString(R.string.android) +
                     "\n3. " + getString(R.string.php) +
                     "\n4. " + getString(R.string.java)}"
+            var myToast = Toast.makeText(this,getString(R.string.android),Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.CENTER,500,0)
+            myToast.show()
         }
     }
 }
